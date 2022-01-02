cf0ccce7679d0611ec7fcbab44be49f62fee5e12
TOXIC DEVIL <toxicdevil.abhinav@gmail.com>
2022-01-02 13:35:06 +0530

    Installer for Android Devices 🥳

---
+++ termux.sh
@@ -0,0 +1,8 @@
+apt update
+apt install nodejs
+apt install git
+git clone https://github.com/TOXIC-DEVL/WhatsAlexa
+cd WhatsAlexa
+npm install @adiwajshing/baileys
+npm install chalk
+node index.js
